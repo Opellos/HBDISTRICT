74e117c68cb876e87caaa0070d332df583c2ec9f
Opellos <hbmailer@online.de>
2025-09-05 01:32:03 +0200

    feat: implement persistent glass navbar

--- 002_packdat/js/main.js
+++ 002_packdat/js/main.js
@@ -71,22 +71,6 @@
         $('.bg-img-custom-small').parallax("50%", .15);
 
         /***********************************************
-         * jQuery to collapse the navbar on scroll
-         ***********************************************/
-
-        $(window).scroll(function () {
-
-            var nav = $('.navbar-Concept');
-            if (nav.length) {
-
-                if ($(".navbar-Concept").offset().top > 50) {
-                    $(".navbar-fixed-top").addClass("top-nav-collapse");
-                } else {
-                    $(".navbar-fixed-top").removeClass("top-nav-collapse");
-                }}
-        });
-
-        /***********************************************
          * Tabs
          ***********************************************/
 
