bf7fbdf9b5587877b2072c1e6bd0adc651231089
Opellos <hbmailer@online.de>
2025-09-07 14:20:27 +0200

    fix: refine navbar height and animations

--- 002_packdat/js/main.js
+++ 002_packdat/js/main.js
@@ -86,9 +86,9 @@
         $(function () {
             $('a.page-scroll').on('click', function (event) {
                 var $anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: ($($anchor.attr('href')).offset().top - 55)
-                }, 1500, 'easeInOutExpo');
+                  $('html, body').stop().animate({
+                      scrollTop: ($($anchor.attr('href')).offset().top - 60)
+                  }, 1500, 'easeInOutExpo');
                 event.preventDefault();
             });
         });
